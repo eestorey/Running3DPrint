eda22dd147af44d813539db4281b3c28194203ec
Emily Storey <emily.e.storey@gmail.com>
2022-01-31 19:58:33 -0500

    Import all routes, dilate them, plot union

--- route_plotting_2.py
+++ route_plotting_2.py
@@ -3,31 +3,31 @@ from gpx_converter import Converter
 import matplotlib.pyplot as plt
 import numpy as np
 from shapely.geometry import LineString
+import shapely.ops as so
+from shapely.ops import unary_union
 
 
-for gpx_file in os.listdir('test_files_for_plotting'):
-    df = Converter(input_file=os.path.join('test_files_for_plotting', gpx_file)).gpx_to_dataframe()
+#%% Union of all routes
+
+routes = []
+routes_dilated = []
+
+for gpx_file in os.listdir('Activities/Activities Checked Renamed'):
+    df = Converter(input_file=os.path.join('Activities/Activities Checked Renamed', gpx_file)).gpx_to_dataframe()
     longitude_latitude_tuples = [tuple(x) for x in df[["longitude", "latitude"]].values]
 
     line = LineString(longitude_latitude_tuples)
     dilated = line.buffer(0.0001)
-    xo,yo = dilated.exterior.xy
-    plt.plot(xo, yo)
 
-    for interior in dilated.interiors:
+    routes.append(line)
+    routes_dilated.append(dilated)
+
+route_unions = unary_union(routes_dilated)
+for geom in route_unions.geoms:
+    xo, yo = geom.exterior.xy
+    plt.fill(xo, yo, "b")
+    for interior in geom.interiors:
         xi,yi = interior.xy
-        plt.plot(xi, yi)
+        plt.fill(xi, yi, "w")
 
 plt.show()
-
-# gpx_file = 'test.gpx'
-# df = Converter(input_file=gpx_file).gpx_to_dataframe()
-# longitude_latitude = df[["longitude", "latitude"]]
-# longitude_latitude_tuples = [tuple(x) for x in longitude_latitude.values]
-
-# line = LineString(longitude_latitude_tuples)
-# dilated = line.buffer(0.00005)
-# x,y = dilated.exterior.xy
-
-# plt.plot(x, y)
-# plt.show()
