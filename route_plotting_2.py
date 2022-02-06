315b5f1098448cafd5d0fd30e1ed04d815d42266
Emily Storey <emily.e.storey@gmail.com>
2022-02-06 14:32:42 -0500

    gets and plots centerlines from the union of all dilated routes

--- route_plotting_2.py
+++ route_plotting_2.py
@@ -2,16 +2,16 @@ import os
 from gpx_converter import Converter
 import matplotlib.pyplot as plt
 import numpy as np
-from shapely.geometry import LineString
+# import shapely.geometry 
+from shapely.geometry import LineString, Polygon, MultiLineString
 import shapely.ops as so
 from shapely.ops import unary_union
+from centerline.geometry import Centerline
 
 #  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 #  .venv\scripts\activate
 
 GPX_DIRECTORY = 'Activities/Test Activities'
-
-#%% Union of all routes
 
 routes = []
 routes_dilated = []
@@ -25,44 +25,31 @@ for gpx_file in os.listdir(GPX_DIRECTORY):
 
     routes.append(line)
     routes_dilated.append(dilated)
-#%%
 
 route_unions = unary_union(routes_dilated)
 if route_unions.type == 'MultiPolygon':
     for geom in route_unions.geoms:
         xo, yo = geom.exterior.xy
-        plt.plot(xo, yo, "b")
+        plt.plot(xo, yo, "cornflowerblue")
         for interior in geom.interiors:
             xi,yi = interior.xy
-            plt.plot(xi, yi, "b")
-else: 
+            plt.plot(xi, yi, "cornflowerblue")
+else:
     xo, yo = route_unions.exterior.xy
-    plt.plot(xo, yo, "b")
+    plt.plot(xo, yo, "cornflowerblue")
     for interior in route_unions.interiors:
         xi,yi = interior.xy
-        plt.plot(xi, yi, "b")
+        plt.plot(xi, yi, "cornflowerblue")
+
+    centerline = Centerline(route_unions, interpolation_distance=0.00007)
+    for cl_segment in list(centerline.geoms):
+        xs, ys = cl_segment.xy
+        plt.plot(xs, ys, 'r')
 
 plt.show()
 
-# Next step is to install and use the package 'centerlines'. Issue is that I am having trouble installing it, the site where I can get the whl file from for gdal is timing out. 
-# Same site I got shapely from, so it should be working... Just not at the moment. 
-# https://www.lfd.uci.edu/~gohlke/pythonlibs/#gdal 
-# I need version cp310 win64. 
-# https://stackoverflow.com/questions/43587960/gdal-installation-error-using-pip
-# https://gis.stackexchange.com/questions/210586/attempting-to-install-pip-package-produces-gdalversion-is-not-defined-error
-
-
-
-
-# Next step... Find areas of the union that are only in one polygon.
-# trace the line that is on the borders of those and the rest?
-
-# Or... trace the lines and then also trace the midlines within a certain tolerance?
+# Next step is to use the package 'centerlines'. 
 # then use normals to the midlines to bisect the regions?
-
-# probably realistically the next step is to go back to test regions for now, 
-# plot them all with some alpha along with the routes. See what sort of operations
-# need to be done. 
 
 # Or hey what if I just took the entire polygon and made cells within it and then
 # tailor the cell size so that it captures common routes but also separates nearby
@@ -70,4 +57,7 @@ plt.show()
 
 # https://stackoverflow.com/questions/25656120/how-to-check-if-two-gps-routes-are-equals
 # https://stackoverflow.com/questions/68348779/given-a-polygon-line-in-it-find-position-on-line-where-to-split-the-polygon-w
-# 
+
+
+# count overlapping polygon regions? 
+# https://stackoverflow.com/questions/66341143/count-overlapping-features-using-geopandas
