97f7eff99c2fbe86ff03d67355c6abea2481f412
Emily Storey <emily.e.storey@gmail.com>
2022-02-06 21:05:06 -0500

    dilate then erode before centerline calculation

--- route_plotting_2.py
+++ route_plotting_2.py
@@ -5,6 +5,7 @@ import numpy as np
 # import shapely.geometry 
 from shapely.geometry import LineString, Polygon, MultiLineString
 import shapely.ops as so
+from shapely.ops import snap
 from shapely.ops import unary_union
 from centerline.geometry import Centerline
 
@@ -12,41 +13,82 @@ from centerline.geometry import Centerline
 #  .venv\scripts\activate
 
 GPX_DIRECTORY = 'Activities/Test Activities'
+OFFSET_DISTANCE = 0.0001 # this has to be calculated based on the canvas size and minimum nozzle. want each line to be at least 2 nozzle widths wide. 
+INTERPOLATION_DISTANCE = OFFSET_DISTANCE * 2/3
 
 routes = []
 routes_dilated = []
+routes_eroded = []
 
 for gpx_file in os.listdir(GPX_DIRECTORY):
     df = Converter(input_file=os.path.join(GPX_DIRECTORY, gpx_file)).gpx_to_dataframe()
     longitude_latitude_tuples = [tuple(x) for x in df[["longitude", "latitude"]].values]
 
     line = LineString(longitude_latitude_tuples)
-    dilated = line.buffer(0.0001)
+    xl, yl = line.xy
+    plt.plot(xl, yl, 'gray')
+
+    dilated = line.buffer(OFFSET_DISTANCE)
 
     routes.append(line)
     routes_dilated.append(dilated)
 
-route_unions = unary_union(routes_dilated)
-if route_unions.type == 'MultiPolygon':
-    for geom in route_unions.geoms:
+route_unions_dilated = unary_union(routes_dilated)
+route_unions_eroded = route_unions_dilated.buffer(-0.75*OFFSET_DISTANCE)
+
+if route_unions_dilated.type == 'MultiPolygon':
+    for geom in route_unions_dilated.geoms:
         xo, yo = geom.exterior.xy
-        plt.plot(xo, yo, "cornflowerblue")
+        # plt.plot(xo, yo, "cornflowerblue")
         for interior in geom.interiors:
             xi,yi = interior.xy
             plt.plot(xi, yi, "cornflowerblue")
 else:
-    xo, yo = route_unions.exterior.xy
-    plt.plot(xo, yo, "cornflowerblue")
-    for interior in route_unions.interiors:
-        xi,yi = interior.xy
-        plt.plot(xi, yi, "cornflowerblue")
+    xo, yo = route_unions_dilated.exterior.xy
+    plt.fill(xo, yo, "cornflowerblue")
 
-    centerline = Centerline(route_unions, interpolation_distance=0.00007)
+    xo, yo = route_unions_eroded.exterior.xy
+    plt.fill(xo, yo, "white")
+
+    for interior in route_unions_eroded.interiors:
+        xi,yi = interior.xy
+        plt.fill(xi, yi, "cornflowerblue")
+
+    for interior in route_unions_dilated.interiors:
+        xi,yi = interior.xy
+        plt.fill(xi, yi, "white")
+
+    centerline = Centerline(route_unions_eroded, interpolation_distance=INTERPOLATION_DISTANCE)
+
     for cl_segment in list(centerline.geoms):
         xs, ys = cl_segment.xy
         plt.plot(xs, ys, 'r')
 
+    # this didn't work very well at all. 
+    # result = snap(routes[0], centerline, OFFSET_DISTANCE)
+    # xi, yi = result.xy
+    # plt.plot(xi, yi, 'lime')
+
 plt.show()
+
+
+
+
+
+
+# Things are working okay for now. What I want to do next is to join the linestrings that make up the centerlines into different linestrings between each intersection. 
+# And also want to smooth the linestrings. They are jagged. 
+# this will help smooth 
+# https://gis.stackexchange.com/questions/231233/create-smooth-line-from-zigzag-line
+
+
+
+
+
+
+# below this is shitballing and ideas. 
+
+
 
 # Next step is to use the package 'centerlines'. 
 # then use normals to the midlines to bisect the regions?
@@ -61,3 +103,17 @@ plt.show()
 
 # count overlapping polygon regions? 
 # https://stackoverflow.com/questions/66341143/count-overlapping-features-using-geopandas
+
+# separate different 'streets'
+# https://gis.stackexchange.com/questions/98087/split-lines-at-intersection-of-other-lines
+# this is what i WANT https://community.esri.com/t5/python-questions/split-road-edge-polygon-at-intersections/td-p/20031
+
+# what if... I count overlapping polygon regions and then work incrementally. first find regions where overlapping count == 1. 
+# but make sure it's not picking out little slivers. Must have a "width" approximately twice the offset distance... 
+
+# i guess a few options are to use shapely snapping or shapely shared paths or shapely split. 
+# All 3 are located around https://shapely.readthedocs.io/en/stable/manual.html#shapely.ops.snap
+
+
+# https://www.youtube.com/watch?v=t6gGrv3vRoM 
+
